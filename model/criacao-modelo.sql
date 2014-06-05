e249affb0435092e75a2ed18ce87c7cb094757a4
Rodrigo Hjort <rodrigo.hjort@gmail.com>
2014-06-05 11:25:17 -0300

    criar e popular tabelas no PG para armazenar conteúdo dos arquivos XYT (samples/xyt)

---
+++ model/criacao-modelo.sql
@@ -0,0 +1,15 @@
+psql afis -c "
+DROP TABLE IF EXISTS dedos;
+CREATE TABLE dedos (
+  id serial not null primary key,
+  arq varchar not null,
+  xyt text
+);"
+
+cd ../samples/xyt
+for arq in *.xyt
+do
+  psql afis -c "INSERT INTO dedos (arq, xyt) VALUES ('$arq', '`cat $arq`')"
+done
+cd -
+
