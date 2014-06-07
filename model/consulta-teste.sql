ed9c1fb704c4342dc649f9b4487702d84458a3ad
Rodrigo Hjort <rodrigo.hjort@gmail.com>
2014-06-07 01:17:30 -0300

    Documentar é sempre bom!

--- model/consulta-teste.sql
+++ model/consulta-teste.sql
@@ -1,14 +1,28 @@
+SELECT a.arq AS arq1, b.arq AS arq2,
+  bz_match(a.xyt, b.xyt) AS match
+FROM dedos a, dedos b
+WHERE a.id = 1
+  AND bz_match(a.xyt, b.xyt) > 20
+ORDER BY match DESC;
+
+/*
+afis=# SELECT a.arq AS arq1, b.arq AS arq2, bz_match(a.xyt, b.xyt) AS match FROM dedos a, dedos b WHERE a.id = 1 AND bz_match(a.xyt, b.xyt) > 20 ORDER BY match DESC;
+   arq1    |   arq2    | match 
+-----------+-----------+-------
+ 101_1.xyt | 101_1.xyt |   144
+ 101_1.xyt | 101_6.xyt |    40
+ 101_1.xyt | 101_8.xyt |    37
+ 101_1.xyt | 101_2.xyt |    24
+(4 rows)
+*/
+
 SELECT b.arq, bz_match(a.xyt, b.xyt) AS match
 FROM dedos a, dedos b
 WHERE a.id = 1
 ORDER BY match DESC;
 
 /*
-afis=# SELECT b.arq, bz_match(a.xyt, b.xyt) AS match
-FROM dedos a, dedos b
-WHERE a.id = 1
-ORDER BY match DESC
-LIMIT 5;
+afis=# SELECT b.arq, bz_match(a.xyt, b.xyt) AS match FROM dedos a, dedos b WHERE a.id = 1 ORDER BY match DESC LIMIT 5;
     arq    | match 
 -----------+-------
  101_1.xyt |   144
