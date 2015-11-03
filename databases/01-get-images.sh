21525db647f11ea5342f9b71b7b3113c5ce591ad
Rodrigo Hjort - DP/CETEC/CTCID <03323620941@serpro-1549776.(none)>
2015-11-03 09:58:06 -0200

    Correction of issues in the script

--- databases/01-get-images.sh
+++ databases/01-get-images.sh
@@ -2,10 +2,21 @@
 
 # source: http://bias.csr.unibo.it/fvc2004/download.asp
 
+# criar diretório para os ZIPs
+if [ ! -d zips ]
+then
+  mkdir zips
+fi
+
 # baixar arquivos
 for i in `seq 1 4`
 do
-  wget "http://bias.csr.unibo.it/fvc2004/Downloads/DB${i}_B.zip"
+  arq="DB${i}_B.zip"
+  if [ ! -f zips/$arq ]
+  then
+    wget "http://bias.csr.unibo.it/fvc2004/Downloads/${arq}"
+    mv $arq zips/
+  fi
 done
 
 # excluir diretórios
