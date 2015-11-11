0bf9c2a3e8753486603c2be344f6559e61a6b029
Rodrigo Hjort <rodrigo.hjort@gmail.com>
2015-11-11 18:00:39 -0200

    Created scripts for evaluating FRR and FAR rates

--- databases/casia/08-error-tests-casia.sql
+++ databases/casia/08-error-tests-casia.sql
@@ -59,14 +59,18 @@ score >= 80
 
 -- FRR (False Rejection Rate)
 
-SELECT total_correctly_accepted, total_matches_performed,
-  (1 - total_correctly_accepted::numeric / total_matches_performed) * 100 AS false_rejection_rate
+SELECT total_correctly_accepted, total_correctly_expected, total_matches_performed,
+  (1 - total_correctly_accepted::numeric / total_correctly_expected) * 100 AS false_rejection_rate
 FROM (
   SELECT count(x.*) AS total_correctly_accepted, (
     SELECT count(*) FROM casia a, casia b WHERE b.id > a.id
       --AND a.pid = 1 AND a.fid = 'R0' AND b.pid = 1 AND b.fid = 'R0' -- for debugging
       --AND a.pid = 1 AND a.fid IN ('R0', 'L0') AND b.pid = 1 AND b.fid IN ('R0', 'L0') -- for debugging
-    ) AS total_matches_performed
+    ) AS total_matches_performed, (
+    SELECT count(*) FROM casia a, casia b WHERE b.id > a.id AND a.pid = b.pid AND a.fid = b.fid
+      --AND a.pid = 1 AND a.fid = 'R0' AND b.pid = 1 AND b.fid = 'R0' -- for debugging
+      --AND a.pid = 1 AND a.fid IN ('R0', 'L0') AND b.pid = 1 AND b.fid IN ('R0', 'L0') -- for debugging
+    ) AS total_correctly_expected
   FROM (
     SELECT a.id, a.pid, a.fid, /*b.probe,*/ b.score, /*b.sample,*/ c.id, c.pid, c.fid
     FROM casia a
